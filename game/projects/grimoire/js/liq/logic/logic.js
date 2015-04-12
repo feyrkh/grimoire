02860ea29e57c7eedbd6e957fedc2952620244e8
Kevin Hobbs <kevinhobbs@gmail.com>
2015-04-12 13:57:20 -0500

    Adding theorem tests and precept jsdoc.

--- game/projects/grimoire/js/liq/logic/logic.js
+++ game/projects/grimoire/js/liq/logic/logic.js
@@ -1,6 +1,8 @@
 liq.logic = {};
 
 pc.jsLoader.add('liq/logic/precept.js');
+pc.jsLoader.add('liq/logic/theorem.js');
 
 
 pc.jsLoader.add('liq/logic/precept-test.js');
+pc.jsLoader.add('liq/logic/theorem-test.js');
