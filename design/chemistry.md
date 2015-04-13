4b27774ef234a195784f2d58cd9f7f664613e9be
Kevin Hobbs <khobbs@homeaway.com>
2015-04-13 14:43:20 -0500

    Chemistry updates.

---
+++ design/chemistry.md
@@ -0,0 +1,31 @@
+= Chemistry
+== Phases of precept matter
+* Solid - Melted to liquid, used as catalyst in liquid or gas, transmuted by light
+* Liquid - Frozen to solid, heated to gas, temper's pure light into alchemical light
+* Gas - Condensed to liquid, temper's pure light into alchemical light
+* Light -
+
+== Tools
+* Glass containers + pipes
+* Sealed jars - contain liquids, solids and gases
+* Heat/cold source
+* Light source - Produces a beam of pure light
+* Lens - Hollow lens, if filled with liquid or gas precept it will transmute pure light that flows through it.
+* Prism - Splits alchemical light into its component precepts
+* Centrifuge - Separates a combined liquid/gas into layers based on mass
+* Pressurizer - For high-pressure infusing or condensing, can increase or decrease pressure
+* Mixer - Thoroughly mix substances which might otherwise tend to settle
+* Filter - Captures particles of precept matter > than a specified mass and pushes them into a side flask.
+
+== Precept matter stats
+* Melting/freezing point: Temp at which (under 1 atmosphere of pressure) a solid/liquid transitions to liquid/solid.
+* Boiling/condensation point: Temp at which (under 1 atmosphere of pressure) a liquid/gas transitions to gas/liquid.
+* Splitting point: Temp at which (under 1 atmosphere) a gas splits into its component precepts.
+* Pressure sensitivity: Change in melting/freezing/evaporation/condensation points as pressure goes up/down.
+Pressure sensitivity is in degrees per centiatmosphere.
+* Mass: Weight of the substance. Mainly determines centrifuge order. For non-basic precepts, weight = sum(component weights)
+* Energy: Amount of energy in this substance. When splitting precepts, heat is released if the sum(component energy) is less
+than this precept's energy - 1 degree per energy difference per volume split. If sum(component energy) is greater than this
+precept's energy, the temperature of the resulting products are reduced by the the same amount. When combining, temp
+increases if the product's energy is less than the component sum, or decreases if it's greater.
+* Temperature: Current temperature of the substance. Temperature equalizes between adjacent drops of liquid over time.
--- game/projects/grimoire/js/liq/logic/precept.js
+++ game/projects/grimoire/js/liq/logic/precept.js
@@ -1,5 +1,33 @@
-(function() {
+(function () {
     var registry = {};
+
+    function randomAmt(amt) {
+        if (typeof(amt) === 'string' && amt.indexOf(':') >= 0) {
+            var randRange = amt.split(':');
+            var min = Number(randRange[0]);
+            var max = Number(randRange[1]);
+            amt = min + Number(Math.random() * (max - min));
+        } else {
+            amt = Number(amt);
+        }
+
+        return amt;
+    }
+
+    function settingsOrDefault(settings, defaults, field) {
+        if (typeof(settings[field] !== 'undefined')) {
+            defaults[field] = settings[field];
+        }
+    }
+
+    function variableField(settings, defaults, field) {
+        var fieldVal = settingsOrDefault(settings, defaults, field);
+        var varVal = settingsOrDefault(settings, defaults, field + 'Variance');
+        fieldVal = randomAmt(fieldVal) + randomAmt(varVal);
+        defaults[field] = fieldVal;
+        defaults[field + 'Variance'] = 0;
+    }
+
     var Precept = pc.MyBase.extend('liq.logic.Precept',
         /** @lends liq.logic.Precept **/
         {
@@ -12,7 +40,7 @@
              * @param dataResource A pc.DataResource containing the JSON to load.
              * @returns An array of liq.logic.Precept instances, or an exception if the precepts are inconsistent
              */
-            loadFile: function(dataResource) {
+            loadFile: function (dataResource) {
                 this.info('Loading ' + dataResource.name + ' into precept registry');
                 var preceptArr = JSON.parse(dataResource.data);
                 for (var preceptIdx in preceptArr) {
@@ -31,6 +59,19 @@
             p2: null,
             desc: null,
             difficulty: null,
+            freezingPoint: 0,
+            freezingPointVariance: '-40:40',
+            boilingPoint: 100,
+            boilingPointVariance: '-40:40',
+            highPressureSensitivity: 0.037,
+            highPressureSensitivityVariance: '-0.01:0.01',
+            lowPressureSensitivity: -0.3,
+            highPressureSensitivityVariance: '-0.1:0.1',
+            energy: 1,
+            energyVariance: '-0.9:0.9',
+            mass: 1,
+            massVariance: '-0.9:3',
+
             /**
              * Array of precept names this precept is a component of.
              */
@@ -49,7 +90,7 @@
              * @param {string} settings.p2 Name of the second parent precept
              * @param {string} settings.desc Flavor text for this precept
              */
-            init: function(settings) {
+            init: function (settings) {
                 this.assertNotEmpty(settings.name, 'Name required');
                 this.assertNotEmpty(settings.desc, 'Desc required');
                 this.assertEmpty(Precept.registry[settings.name], 'Duplicate precept not allowed: ' + settings.name);
@@ -75,10 +116,18 @@
                 this.desc = settings.desc;
                 this.difficulty = this.calculateDifficulty();
                 Precept.registry[this.name] = this;
+
+                variableField(settings, this, 'freezingPoint');
+                variableField(settings, this, 'boilingPoint');
+                variableField(settings, this, 'highPressureSensitivity');
+                variableField(settings, this, 'lowPressureSensitivity');
+                variableField(settings, this, 'energy');
+                variableField(settings, this, 'mass');
+
                 return this;
             },
 
-            calculateDifficulty: function() {
+            calculateDifficulty: function () {
                 var total = 1;
                 if (this.p1 != null) {
                     total += Precept.registry[this.p1].difficulty;
